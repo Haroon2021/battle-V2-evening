1f7c033e8cc753feb788f21e25206e0acdf72c72
Haroon <haroon20@hotmail.co.uk>
2022-02-07 21:42:15 +0000

    step 8 complete

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require "sinatra/reloader" if development?
+
+get '/' do 
+    "Hello!"
+end
+
+get '/secret' do
+  'secret message!!!!!!!'
+end
+
+
+
+get '/cat' do
+  "<div>
+  <div style='border: 3px dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png'> 
+  </div>"
+
+end
+
+
+
+
+
