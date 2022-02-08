67e70178774eac33f7249b105cb0e6a852e86fa0
Haroon <haroon20@hotmail.co.uk>
2022-02-08 13:16:40 +0000

    Step 12 complete sinatra_introducing_param

--- app.rb
+++ app.rb
@@ -11,12 +11,19 @@ end
 
 
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Misty", "Almond"].sample
   erb(:index)
 
 end
 
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  erb :index
+
+end
 
 
 
